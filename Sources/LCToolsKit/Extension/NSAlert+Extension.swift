93cb13db65ab472fd242892ef5efd69676d30f4d
Marvin <93428739@qq.com>
2025-03-31 20:57:48 +0800

    The modifications are as follows
    - Deleted the independent library
    - Optimized NSAlert
    - Added URL method to open the website and add parameters and encode

--- Sources/LCToolsKit/Extension/NSAlert+Extension.swift
+++ Sources/LCToolsKit/Extension/NSAlert+Extension.swift
@@ -10,77 +10,84 @@ import AppKit
 
 public extension NSAlert {
     
-    /// 显示 alert
+    /// 显示alert
     /// - Parameters:
-    ///   - modalWindow: 要展示的目标 window
+    ///   - modalWindow: 要展示的目标window
     ///   - title: 标题
     ///   - message: 显示内容
-    ///   - accessoryView: 插入自定义的 view
-    ///   - buttons: 按钮，最多 3 个，从右到左，或者从上到下
+    ///   - accessoryView: 插入自定义的view
+    ///   - buttons: 按钮，最多3个，从右到左，或者从上到下
     ///   - handler: 回调，返回点击按钮的序号
-    class func show(for modalWindow: NSWindow? = nil, title: String, message: String? = nil, accessoryView: NSView? = nil,
-                    buttons: [String], handler: @escaping (Int) -> Void) {
-        let alert = createAlert(title: title, message: message, accessoryView: accessoryView, buttons: buttons)
-        
-        if let window = modalWindow {
-            alert.beginSheetModal(for: window) { response in
-                handler(handleResponse(response))
-            }
-        } else {
-            handler(handleResponse(alert.runModal()))
-        }
-    }
-    
-    /// 显示带复选框的 alert
-    /// - Parameters:
-    ///   - modalWindow: 要展示的目标 window
-    ///   - title: 标题
-    ///   - message: 显示内容
-    ///   - accessoryView: 插入自定义的 view
-    ///   - checkbox: 插入 suppressionButton 复选框
-    ///   - buttons: 按钮，最多 3 个，从右到左，或者从上到下
-    ///   - handler: 回调，返回点击按钮的序号和复选框状态
-    class func show(for modalWindow: NSWindow?,
-                    title: String, message: String?, accessoryView: NSView? = nil,
-                    checkbox: String, buttons: [String], handler: @escaping (Int, Bool) -> Void) {
-        let alert = createAlert(title: title, message: message, accessoryView: accessoryView, buttons: buttons)
-        alert.showsSuppressionButton = true
-        alert.suppressionButton?.title = checkbox
-        
-        let handleAction: (NSApplication.ModalResponse) -> Void = { response in
-            let state = alert.suppressionButton?.state == .on
-            handler(handleResponse(response), state)
-        }
-        
-        if let window = modalWindow {
-            alert.beginSheetModal(for: window, completionHandler: handleAction)
-        } else {
-            handleAction(alert.runModal())
-        }
-    }
-    
-    // MARK: - Private Helper Methods
-    
-    /// 创建通用的 `NSAlert` 对象
-    private static func createAlert(title: String, message: String?, accessoryView: NSView?, buttons: [String]) -> NSAlert {
+    class func show(for modalWindow: NSWindow? = nil, title: String, message: String? = nil, accessoryView: NSView? = nil, buttons: [String], handler: @escaping (Int) -> Void) {
         let alert = NSAlert()
         alert.messageText = title
-        if let message = message {
-            alert.informativeText = message
-        }
-        alert.accessoryView = accessoryView
         alert.alertStyle = .warning
-        buttons.forEach { alert.addButton(withTitle: $0) }
-        return alert
+        if message != nil { alert.informativeText = message! }
+        alert.accessoryView = accessoryView
+        for btn in buttons {
+            alert.addButton(withTitle: btn)
+        }
+        if modalWindow == nil {
+            let response = alert.runModal()
+            switch response {
+            case .alertFirstButtonReturn:   handler(0)
+            case .alertSecondButtonReturn:  handler(1)
+            case .alertThirdButtonReturn:   handler(2)
+            default: break
+            }
+        } else {
+            alert.beginSheetModal(for: modalWindow!) { response in
+                switch response {
+                case .alertFirstButtonReturn:   handler(0)
+                case .alertSecondButtonReturn:  handler(1)
+                case .alertThirdButtonReturn:   handler(2)
+                default: break
+                }
+            }
+        }
     }
     
-    /// 处理 NSAlert 响应值并返回点击按钮的索引
-    private static func handleResponse(_ response: NSApplication.ModalResponse) -> Int {
-        switch response {
-        case .alertFirstButtonReturn:  return 0
-        case .alertSecondButtonReturn: return 1
-        case .alertThirdButtonReturn:  return 2
-        default: return -1
+    /// 显示alert
+    /// - Parameters:
+    ///   - modalWindow: 要展示的目标window
+    ///   - title: 标题
+    ///   - message: 显示内容
+    ///   - accessoryView: 插入自定义的view
+    ///   - checkbox: 插入suppressionButton，复选框
+    ///   - buttons: 按钮，最多3个，从右到左，或者从上到下
+    ///   - handler: 回调，返回点击按钮的序号，复选框的状态
+    class func show(for modalWindow: NSWindow? = nil, title: String, message: String?, accessoryView: NSView? = nil, checkbox: String, buttons: [String], handler: @escaping (Int, Bool) -> Void) {
+        let alert = NSAlert()
+        alert.messageText = title
+        alert.alertStyle = .warning
+        if message != nil { alert.informativeText = message! }
+        alert.accessoryView = accessoryView
+        alert.showsSuppressionButton = true
+        alert.suppressionButton?.title = checkbox
+        for btn in buttons {
+            alert.addButton(withTitle: btn)
+        }
+        if modalWindow == nil {
+            let response = alert.runModal()
+            let state = alert.suppressionButton?.state == .on
+            switch response {
+            case .alertFirstButtonReturn:   handler(0, state)
+            case .alertSecondButtonReturn:  handler(1, state)
+            case .alertThirdButtonReturn:   handler(2, state)
+            default: break
+            }
+        } else {
+            alert.beginSheetModal(for: modalWindow!) { response in
+                let state = alert.suppressionButton?.state == .on
+                switch response {
+                case .alertFirstButtonReturn:   handler(0, state)
+                case .alertSecondButtonReturn:  handler(1, state)
+                case .alertThirdButtonReturn:   handler(2, state)
+                default: break
+                }
+            }
         }
     }
+    
+    
 }
--- Sources/LCToolsKit/LCURLManager/LCURLManager.swift
+++ Sources/LCToolsKit/LCURLManager/LCURLManager.swift
@@ -21,5 +21,23 @@ public class LCURLManager {
     }
     
     
+    /// 打开`搜索 URL`, 并传递`关键字`作为`参数`
+    ///
+    /// - Parameters:
+    ///   - baseURL: 搜索引擎基础 URL
+    ///   - keyword: 搜索关键词
+    public static func openSearchURL(baseURL: String, keyword: String) {
+        // 对关键字进行URL编码，以便在URL中使用
+        if let encodedKeyword = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+            // 构建搜索URL
+            let urlString = baseURL + encodedKeyword
+            if let url = URL(string: urlString) {
+                // 使用NSWorkspace打开URL
+                NSWorkspace.shared.open(url)
+            }
+        }
+    }
+    
+    
     
 }
