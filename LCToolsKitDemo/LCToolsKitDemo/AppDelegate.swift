091d4bcd20dfa2733c3591504f05f9d372fd0826
Marvin <93428739@qq.com>
2025-04-12 19:18:16 +0800

    add demo project

---
+++ LCToolsKitDemo/LCToolsKitDemo/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  LCToolsKitDemo
+//
+//  Created by Liu Chuan on 2025/4/12.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
