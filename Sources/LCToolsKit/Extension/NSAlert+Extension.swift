6607e4d34635d9e15629e905212610da5e6a40fb
Marvin <93428739@qq.com>
2025-04-16 22:14:24 +0800

    Update NSAlert+Extension.swift

--- Sources/LCToolsKit/Extension/NSAlert+Extension.swift
+++ Sources/LCToolsKit/Extension/NSAlert+Extension.swift
@@ -1,7 +1,7 @@
 //
 //  NSAlert+Extension.swift
 //
-//  Created by DevLiuSir on 2023/3/2.
+//  Created by DevLiuSir on 2021/3/2.
 //
 
 
@@ -13,15 +13,16 @@ public extension NSAlert {
     /// 显示alert
     /// - Parameters:
     ///   - modalWindow: 要展示的目标window
+    ///   - style: 警告框的样式
     ///   - title: 标题
     ///   - message: 显示内容
     ///   - accessoryView: 插入自定义的view
     ///   - buttons: 按钮，最多3个，从右到左，或者从上到下
     ///   - handler: 回调，返回点击按钮的序号
-    class func show(for modalWindow: NSWindow? = nil, title: String, message: String? = nil, accessoryView: NSView? = nil, buttons: [String], handler: @escaping (Int) -> Void) {
+    class func show(for modalWindow: NSWindow? = nil, style: NSAlert.Style = .warning, title: String, message: String? = nil, accessoryView: NSView? = nil, buttons: [String], handler: @escaping (Int) -> Void) {
         let alert = NSAlert()
         alert.messageText = title
-        alert.alertStyle = .warning
+        alert.alertStyle = style
         if message != nil { alert.informativeText = message! }
         alert.accessoryView = accessoryView
         for btn in buttons {
@@ -50,16 +51,17 @@ public extension NSAlert {
     /// 显示alert
     /// - Parameters:
     ///   - modalWindow: 要展示的目标window
+    ///   - style: 警告框的样式
     ///   - title: 标题
     ///   - message: 显示内容
     ///   - accessoryView: 插入自定义的view
     ///   - checkbox: 插入suppressionButton，复选框
     ///   - buttons: 按钮，最多3个，从右到左，或者从上到下
     ///   - handler: 回调，返回点击按钮的序号，复选框的状态
-    class func show(for modalWindow: NSWindow? = nil, title: String, message: String?, accessoryView: NSView? = nil, checkbox: String, buttons: [String], handler: @escaping (Int, Bool) -> Void) {
+    class func show(for modalWindow: NSWindow? = nil, style: NSAlert.Style = .warning, title: String, message: String?, accessoryView: NSView? = nil, checkbox: String, buttons: [String], handler: @escaping (Int, Bool) -> Void) {
         let alert = NSAlert()
         alert.messageText = title
-        alert.alertStyle = .warning
+        alert.alertStyle = style
         if message != nil { alert.informativeText = message! }
         alert.accessoryView = accessoryView
         alert.showsSuppressionButton = true
