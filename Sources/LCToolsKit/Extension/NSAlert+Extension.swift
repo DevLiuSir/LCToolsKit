c66477039d196db6038b1294d6587b0382e87cc4
Marvin <93428739@qq.com>
2025-08-21 10:33:44 +0800

    Update NSAlert+Extension.swift

--- Sources/LCToolsKit/Extension/NSAlert+Extension.swift
+++ Sources/LCToolsKit/Extension/NSAlert+Extension.swift
@@ -92,4 +92,68 @@ public extension NSAlert {
     }
     
     
+    /// 显示一个警告对话框
+    /// - Parameters:
+    ///   - title: 标题
+    ///   - message: 显示内容
+    ///   - accessoryView: 插入自定义的view
+    ///   - buttons: 按钮，最多3个，从右到左，或者从上到下
+    /// - Returns: 用户点击的按钮索引（0 表示第一个按钮，1 表示第二个按钮，2 表示第三个按钮，未匹配返回 -1）
+    class func show(title: String, message: String?, accessoryView: NSView? = nil, buttons: [String]) -> Int {
+        let alert = NSAlert()
+        alert.messageText = title
+        alert.alertStyle = .warning
+        if message != nil { alert.informativeText = message! }
+        alert.accessoryView = accessoryView
+        for btn in buttons {
+            alert.addButton(withTitle: btn)
+        }
+        let response = alert.runModal()
+        switch response {
+        case .alertFirstButtonReturn:   return 0
+        case .alertSecondButtonReturn:  return 1
+        case .alertThirdButtonReturn:   return 2
+        default: break
+        }
+        return -1
+    }
+    
+    
+    
+    /// 显示一个带复选框的警告对话框
+    ///
+    /// - Parameters:
+    ///   - title: 对话框标题
+    ///   - message: 显示的提示信息（可选）
+    ///   - accessoryView: 需要插入到对话框中的自定义视图（可选）
+    ///   - checkbox: 复选框文字（suppressionButton 的标题）
+    ///   - buttons: 按钮标题数组，最多支持 3 个，排列顺序为从右到左（水平排列时）或从上到下（垂直排列时）
+    ///
+    /// - Returns: 一个元组 `(index, isChecked)`
+    ///   - `index`: 用户点击的按钮索引（0 表示第一个按钮，1 表示第二个按钮，2 表示第三个按钮，未匹配返回 -1）
+    ///   - `isChecked`: 复选框是否被选中
+    class func show(title: String, message: String?, accessoryView: NSView? = nil, checkbox: String, buttons: [String]) -> (Int, Bool) {
+        let alert = NSAlert()
+        alert.messageText = title
+        alert.alertStyle = .warning
+        if message != nil { alert.informativeText = message! }
+        alert.accessoryView = accessoryView
+        alert.showsSuppressionButton = true
+        alert.suppressionButton?.title = checkbox
+        for btn in buttons {
+            alert.addButton(withTitle: btn)
+        }
+        let response = alert.runModal()
+        let state = alert.suppressionButton?.state == .on
+        switch response {
+        case .alertFirstButtonReturn:   return (0, state)
+        case .alertSecondButtonReturn:  return (1, state)
+        case .alertThirdButtonReturn:   return (2, state)
+        default: break
+        }
+        return (-1, state)
+    }
+    
+    
+    
 }
